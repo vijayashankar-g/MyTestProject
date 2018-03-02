ca4f28773ac8bca2071f04020ef1d9770f520c61
gostock <mail2gostock@gmail.com>
2018-03-01 22:04:38 -0600

    Test Project

---
+++ src/com/test/MyTestClass.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class MyTestClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is the Test Class called from Jenkins");
+		System.out.println("Well.. Hello World !");
+	}
+
+}
